68144929aa2b4a85cf56f2fcad257fc3b4bc686d
Abdullah Bhatti <99602533+Abdullahtsv@users.noreply.github.com>
2022-05-27 19:16:07 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var evenOdd = +prompt("Enter any number to find even or odd number")
+for(i = 0; i <= 0; i++)
+    if(evenOdd % 2 === 0){
+        document.write("even number")
+        break;
+    }    
+       if(evenOdd % 2 !== 0){
+            document.write("odd number")
+        }
